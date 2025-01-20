916635b1b582b7d291e227aac5eb51723ab55b2d
mehedi37m <mz.mehedi37@gmail.com>
2025-01-21 01:58:09 +0600

    onek din por

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-
-
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductPage from "./pages/Productpage";
+import Sidebar from "./components/Sidebar";
 
 
 function App() {
-  
-
   return (
-    <>
-    <div className='bg-slate-600'></div>
-        
-      <h1 className='border '>Vite + React</h1>
-      
-    </>
-  )
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+
+      {/* bg */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"></div>
+        <div className="absolute inset-0 backdrop-blur-sm"></div>   
+              
+    </div>
+
+     <Sidebar></Sidebar>
+      <Routes>
+        <Route path="/" element={<OverviewPage />}></Route>
+        <Route path="/products" element={<ProductPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
